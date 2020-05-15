5cb8a5d1ea8c12df2cba1312c8bdb7bc3c1f8864
Royce <royce.com@gmail.com>
2020-05-15 19:31:48 +0800

    Re-enabled commented tests

--- spec/examples/logic_checker_spec.rb
+++ spec/examples/logic_checker_spec.rb
@@ -4,13 +4,13 @@ require './lib/rast'
 require './examples/logic_checker'
 
 rast LogicChecker do
-  # spec 'Logical AND' do
-  #   execute { |left, right| subject.and(left, right) }
-  # end
+  spec 'Logical AND' do
+    execute { |left, right| subject.and(left, right) }
+  end
 
-  # spec 'Logical OR' do
-  #   execute { |left, right| subject.or(left, right) }
-  # end
+  spec 'Logical OR' do
+    execute { |left, right| subject.or(left, right) }
+  end
 
   spec 'Logical XOR' do
     execute { |left, right| subject.xor(left, right) }
